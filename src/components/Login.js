728e035de88b64a8cf2190cc36d888fc23517e3b
Jean Campelo <jean93campelo@gmail.com>
2022-09-06 14:31:04 -0300

    Criação layout formulário

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,39 @@
-export default function Login () {
-    return (
-        <>
-        Login
-        </>
-    )
+import styled from "styled-components";
+
+export default function Login() {
+  return (
+    <Main>
+      <Form>
+        <input placeholder="E-mail" name="email" />
+        <input placeholder="Senha" name="password" />
+      </Form>
+    </Main>
+  );
 }
+
+const Main = styled.main`
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  align-items: center;
+  justify-content: center;
+  background-color: var(--color-background);
+`;
+
+const Form = styled.form`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  input {
+    width: 70vw;
+    height: 45px;
+    margin-bottom: 6px;
+    font-family: Raleway;
+    color: var(--color-black);
+    border: 1px solid var(--color-background);
+    border-radius: 5px;
+    padding: 10px;
+    font-size: 20px;
+  }
+`;
