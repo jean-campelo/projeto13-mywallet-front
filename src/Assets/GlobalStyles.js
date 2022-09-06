adc7cfa2d93291108ba6a2cfd368c0fc32f56192
Jean Campelo <jean93campelo@gmail.com>
2022-09-06 14:05:45 -0300

    CSS reset e Global Styles

---
+++ src/Assets/GlobalStyles.js
@@ -0,0 +1,59 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed, 
+figure, figcaption, footer, header, hgroup, 
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+	margin: 0;
+	padding: 0;
+	border: 0;
+	font-size: 100%;
+	font: inherit;
+	vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure, 
+footer, header, hgroup, menu, nav, section {
+	display: block;
+}
+body {
+	line-height: 1;
+}
+ol, ul {
+	list-style: none;
+}
+blockquote, q {
+	quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+	content: '';
+	content: none;
+}
+table {
+	border-collapse: collapse;
+	border-spacing: 0;
+}
+* {
+	box-sizing: border-box;
+	--font-primary: Playball;
+	--color-background: #8C11BE;
+    --color-buttons: #A328D6;
+    --color-white: #FFFFFF;
+    --color-green: #03AC00;
+    --color-red: #C70000;
+    --color-black: #000000;
+}
+`;
+
+export default GlobalStyles;
--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,9 @@
+import GlobalStyles from "../Assets/GlobalStyles";
+
 export default function App () {
     return (
         <>
+        <GlobalStyles />
         
         </>
     )
