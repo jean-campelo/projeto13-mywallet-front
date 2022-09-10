374204b15aa2f883014d1d8df064045e6405feca
Jean Campelo <jean93campelo@gmail.com>
2022-09-10 18:43:06 -0300

    Corrigindo rota para cadastro

--- src/services/my_wallet.js
+++ src/services/my_wallet.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const URL = "http://localhost:5000";
 
 function postNewUser (body) {
-    const promise = axios.post(`${URL}/sign-in`, body);
+    const promise = axios.post(`${URL}/sign-up`, body);
     return promise;
 };
 
