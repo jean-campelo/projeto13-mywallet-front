ec3cfc56b052741eb4aa84f81bbe0a69263535c5
Jean Campelo <jean93campelo@gmail.com>
2022-09-12 09:09:45 -0300

    Renderizando informações das transações

--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -1,7 +1,39 @@
-export default function transactions () {
+import styled from "styled-components";
+import DataContext from "../context/dataContext.js";
+import { useContext } from "react";
+
+export default function Transactions() {
+  const { transactions } = useContext(DataContext);
+
+  return (
+    <>
+      {transactions.map((item) => (
+        <Render
+          date={item.date}
+          value={item.value}
+          type={item.type}
+          description={item.description}
+        />
+      ))}
+    </>
+  );
+
+  function Render({ date, value, type, description }) {
     return (
-        <>
-        <h1>Historic</h1>
-        </>
-    )
+      <Register>
+        <Info>
+          <h1> {date} </h1>
+          <h2> {description} </h2>
+        </Info>
+        <Value>
+          <h2> {value} </h2>
+        </Value>
+      </Register>
+    );
+  }
 }
+const Register = styled.div``;
+
+const Info = styled.div``;
+
+const Value = styled.div``;
